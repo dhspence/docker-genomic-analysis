d4f447f1c6d681a49fa9565ba96cbff64391e5dd
dhspence <dhspence@gmail.com>
2017-11-21 11:42:13 -0600

    Update rpackages.R

--- rpackages.R
+++ rpackages.R
@@ -1,4 +1,4 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite()
 biocLite("GenVisR")
-install.packages(c("tidyverse","ggplot2","Hmisc","plotrix","png","GenomicAlignments","bsseq","scales","DEseq2"))
+install.packages(c("tidyverse","ggplot2","Hmisc","plotrix","png","GenomicAlignments","bsseq","scales","DEseq2","vcfR","scales","data.table"))
